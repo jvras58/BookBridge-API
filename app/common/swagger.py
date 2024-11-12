50a9579d08a1eae279f6e2f90dc28fbb73bdff23
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-11-12 02:05:25 +0000

    Atualizar a rota de documentação da API para a raiz

--- app/common/swagger.py
+++ app/common/swagger.py
@@ -6,7 +6,7 @@ api = Api(
     version='1.0',
     title='API - BookBridge',
     description='Uma API simples de gerenciamento de clubes de leitura',
-    doc='/docs',
+    doc='/',
     security='Bearer Auth',
     authorizations={
     'Bearer Auth': {
